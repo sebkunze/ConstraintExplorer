18ec15d9867ae8f6690aff643f3f16cf36c51f0e
Sebastian Kunze <sebastian.kunze@hh.se>
2017-01-18 15:29:24 +0100

    Modified ConstraintSolver's structure

---
+++ core/object/parser.py
@@ -0,0 +1,6 @@
+# parses TerminatedSymbooglixStates into Programs
+from core.utils import logger
+
+def to_program():
+    logger.info('Parsing TerminatedSymbooglixStates to Program')
+    return 'undefined'
